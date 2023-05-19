14629212c088b17dd4cf4d62157491c40fd915b3
Vori0l <semenyshynnn@gmail.com>
2023-05-19 03:11:39 +0100

    add

--- 07 Task/07main.js
+++ 07 Task/07main.js
@@ -1,22 +1,22 @@
 //1
-// let myWindow = window.open('', '', 'width=300, height=300');
-// setTimeout(function () {
-// 	myWindow.resizeTo(500, 500);
-// }, 2000);
-// setTimeout(function () {
-// 	myWindow.moveTo(200, 200);
-// }, 4000);
-// setTimeout(function () {
-// 	myWindow.close();
-// }, 6000);
+let myWindow = window.open('', '', 'width=300, height=300');
+setTimeout(function () {
+	myWindow.resizeTo(500, 500);
+}, 2000);
+setTimeout(function () {
+	myWindow.moveTo(200, 200);
+}, 4000);
+setTimeout(function () {
+	myWindow.close();
+}, 6000);
 //2
-// document.getElementById(id).style.property = new sty
-// function changeCSS() {
-// 	let change = document.getElementById('text');
-// 	change.style.color = 'orange';
-// 	change.style.fontSize = '20px';
-// 	change.style.fontFamily = 'Comic Sans MS'
-// }
+document.getElementById(id).style.property = new sty
+function changeCSS() {
+	let change = document.getElementById('text');
+	change.style.color = 'orange';
+	change.style.fontSize = '20px';
+	change.style.fontFamily = 'Comic Sans MS'
+}
 //3
 function changebackgroundblue() {
 	document.body.style.backgroundColor = 'blue'
