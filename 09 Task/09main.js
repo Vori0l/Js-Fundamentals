74e9d7ca7dc670019b4e3177bacc02d0ec258d77
Vori0l <semenyshynnn@gmail.com>
2023-05-23 22:12:56 +0100

    09 task

--- 09 Task/09main.js
+++ 09 Task/09main.js
@@ -1,13 +1,19 @@
 ///------1------///
-$('h2.head').css("color", "green");
-$('h2.head').find('.inner').addClass('big_font');
+$(document).ready(function () {
+	$('h2.head').css("color", "green");
+	$('h2.head').find('.inner').addClass('big_font');
+});
 ///------2------///
-$('a[href^="https://"]').attr('target', '_blank');
+$(document).ready(function () {
+	$('a[href^="https://"]').attr('target', '_blank');
+});
 ///------3------///
-$('div.thurd_task div:first').insertBefore('.thurd_task h3:first');
-$('div.thurd_task div:last').insertBefore('.thurd_task h3:last')
+$(document).ready(function () {
+	$('div.thurd_task div:first').insertBefore('.thurd_task h3:first');
+	$('div.thurd_task div:last').insertBefore('.thurd_task h3:last')
+});
 ///------4------///
-$(function () {
+$(document).ready(function () {
 	$('input[type="checkbox"]').change(function () {
 		var checkedCount = $('input[type="checkbox"]:checked').length;
 		if (checkedCount >= 3) {
